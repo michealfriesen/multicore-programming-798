ab6fff17482837343679441e552a38795db8f7f0
Micheal Friesen <micheal@mantou.rcs.uwaterloo.ca>
2019-09-09 16:28:28 -0400

    Added script to parse a single text file, and a script to parse the current directory

---
+++ a1/parse_files.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/bash
+rm results.csv;
+for file in ./*.txt 
+do
+    (cat "$file" | grep -E "^(DS_TYPENAME|TOTAL_THREADS|MAXKEY|INS|DEL|prefill_elapsed_ms|tree_stats_numNodes|total_queries|query_throughput|total_inserts|total_deletes|update_throughput|total_ops|total_throughput)+=[a-zA-Z0-9\.\_]*$" | cut -d"=" -f2 | tr "\n" ","; echo ) >> results.csv ;
+done
