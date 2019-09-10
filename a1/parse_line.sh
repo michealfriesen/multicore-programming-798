50f68d0f395e66a61817bed08144eaacbeea6041
Micheal Friesen <micheal@mantou.rcs.uwaterloo.ca>
2019-09-10 17:30:46 -0400

    Stopped parse_line from making file, it only prints now

--- a1/parse_line.sh
+++ a1/parse_line.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(cat "$1" | grep -E "^(DS_TYPENAME|TOTAL_THREADS|MAXKEY|INS|DEL|prefill_elapsed_ms|tree_stats_numNodes|total_queries|query_throughput|total_inserts|total_deletes|update_throughput|total_ops|total_throughput)+=[a-zA-Z0-9\.\_]*$" | cut -d"=" -f2 | tr "\n" ","; echo ) | tee -a results.csv ;
+(cat "$1" | grep -E "^(DS_TYPENAME|TOTAL_THREADS|MAXKEY|INS|DEL|prefill_elapsed_ms|tree_stats_numNodes|total_queries|query_throughput|total_inserts|total_deletes|update_throughput|total_ops|total_throughput)+=[a-zA-Z0-9\.\_]*$" | cut -d"=" -f2 | tr "\n" ","; echo ) ;
