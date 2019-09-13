958b43b1f2ca4c02b8fb6ebbd84d3869617ad9e5
Micheal Friesen <michealfriesen@gmail.com>
2019-09-13 16:25:17 -0400

    Update to graph maker

--- a2/graph-maker.py
+++ a2/graph-maker.py
@@ -1,14 +1,30 @@
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Variable for the test
 testTime = 1000
-threadCountList = [1, 2, 4, 8]
+threadCountList = [8, 16, 32, 64, 128, 256]
 testTypeList = ["naive", "lock", "faa"]
 
 resultsDict = {}
 
 def bashScriptMaker(threadCount, testTime, testType):
     return """./workload_timed.out %d %d %s | grep increments/s: | cut -d ":" -f2 | awk '{$1=$1};1' """ % (threadCount, testTime, testType)
+
+def graphMaker(dataDict):
+    plt.title('Experiment Results')
+    palette = plt.get_cmap('Set1')
+    plt.xlabel('Thread Count')
+    plt.ylabel('Total Throughput')
+    
+    num = 0 # Allows for color change for each data type
+    for dataType, data in dataDict.items():
+        plt.plot(data[0], data[1], marker='.', color=palette(num), linewidth=1, label=dataType)
+        num += 1
+
+    plt.legend()
+    plt.show()
 
 def main():
     for testType in testTypeList:
@@ -17,7 +33,6 @@ def main():
             if testType not in resultsDict:
                 resultsDict[testType] = []
             print("Adding thread count %d for test type %s" % (threadCount, testType))
-            resultsDict[testType].append(int(os.popen((bashScriptMaker(threadCount, testTime, testType))).read()))
-    
+            resultsDict[testType].append((int(os.popen((bashScriptMaker(threadCount, testTime, testType))).read()), threadCount))
     print(resultsDict)
 main()
