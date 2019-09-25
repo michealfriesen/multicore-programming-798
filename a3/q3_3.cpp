81a132d35705d6929de25f6882809e1f6d13580e
Micheal Friesen <RIFT-DOMAIN\m4friese@rift.multicore.cs.uwaterloo.ca>
2019-09-24 20:40:10 -0400

    Completed memory fence implementation

--- a3/q3_3.cpp
+++ a3/q3_3.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <thread>
 using namespace std;
-
 #define TOTAL_INCREMENTS 100000000
 
 __thread int tid;
 
 class mutex_t {
 private:
+    bool enterArray[2];
+    volatile int turn;
 public:
-    mutex_t() {
+    mutex_t() : turn(0) {
+        enterArray[0] = false;
+        enterArray[1] = false;  
     }
     void lock() {
+        enterArray[tid] = true;
+        __sync_synchronize(); 
+        while(enterArray[1 - tid]) {
+            __sync_synchronize();
+            if(turn != tid){
+                enterArray[tid] = false;
+                __sync_synchronize(); 
+                while(turn != tid){
+                    // Do nothing... (Busy Wait)
+                }
+                enterArray[tid] = true;
+            }
+        } 
     }
     void unlock() {
+        enterArray[tid] = false;
+        turn = 1 - tid;
     }
 };
 
@@ -23,12 +41,11 @@ private:
     mutex_t m;
     volatile int v;
 public:
-    counter_locked() {
-        
-    }
-
+    counter_locked() : v(0){}
+    
     void increment() {
         m.lock();
+        __sync_synchronize(); 
         v++;
         m.unlock();
     }
