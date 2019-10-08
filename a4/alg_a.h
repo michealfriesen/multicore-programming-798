5c78f4261ba080823c2f1c08df4b6e4dbca83ff8
Micheal Friesen <micheal@mantou.rcs.uwaterloo.ca>
2019-10-08 18:11:05 -0400

    Something is broken, but I have to go home

--- a4/alg_a.h
+++ a4/alg_a.h
@@ -2,7 +2,15 @@
 #include "util.h"
 #include <atomic>
 #include <mutex>
+#include <iostream>
 using namespace std;
+
+struct paddedData {
+    char padding2[PADDING_BYTES];
+    atomic<uint64_t> d;
+    mutex m;
+    char padding3[PADDING_BYTES];
+};
 
 class AlgorithmA {
 public:
@@ -12,6 +20,7 @@ public:
     const int numThreads;
     int capacity;
     char padding2[PADDING_BYTES];
+    paddedData * data;
 
     AlgorithmA(const int _numThreads, const int _capacity);
     ~AlgorithmA();
@@ -29,30 +38,71 @@ public:
  */
 AlgorithmA::AlgorithmA(const int _numThreads, const int _capacity)
 : numThreads(_numThreads), capacity(_capacity) {
-    
+	data = new paddedData[capacity];
+    for (int i = 0; i < _capacity; i++)
+        data[i].d = NULL; // Initalize the data structure.
 }
 
 // destructor: clean up any allocated memory, etc.
 AlgorithmA::~AlgorithmA() {
-
+    delete data;
 }
 
 // semantics: try to insert key. return true if successful (if key doesn't already exist), and false otherwise
 bool AlgorithmA::insertIfAbsent(const int tid, const int & key) {
-    return false;
+    int32_t h = murmur3(key); // Generate hash that is indexed to our array.
+    for (int i = 0; i < capacity; i++) {
+        int index = (h + i) % capacity;
+        data[index].m.lock(); // Locking to check if it is the correct value
+        if(data[index].d == key) {
+            data[index].m.unlock();
+            return false;
+        }
+        if(data[index].d == NULL) { // Empty
+            data[index].d = key;
+            data[index].m.unlock();
+            return true;
+        }
+        data[index].m.unlock();
+    }
+    return false; // Return false if there was no space, and the key wasn't found.
 }
 
 // semantics: try to erase key. return true if successful, and false otherwise
 bool AlgorithmA::erase(const int tid, const int & key) {
-    return false;
+    int32_t h = murmur3(key); // Generate hash that is indexed to our array.
+    for (int i = 0; i < capacity; i++) {
+        int index = (h + i) % capacity;
+        data[index].m.lock(); // Locking to check if it is the correct value
+        if(data[index].d == key) {
+            data[index].d = TOMBSTONE;
+            data[index].m.unlock();
+            return true;
+        }
+        if(data[index].d == NULL) { // Empty
+            data[index].m.unlock();
+            return false;
+        }
+        data[index].m.unlock();
+    }
+    return false; // Return false if there was no space, and the key wasn't found.
 }
 
 // semantics: return the sum of all KEYS in the set
 int64_t AlgorithmA::getSumOfKeys() {
-    return 0;
+	// This is the naive way of adding all the values.
+	int64_t sum = 0;
+	for (int i = 0; i < capacity; i++) {
+        data[i].m.lock();
+        if(!(data[i].d == TOMBSTONE)) // Make sure the data is not deleted.
+		    sum += data[i].d;
+        data[i].m.unlock();
+    }
+	return sum;
 }
 
 // print any debugging details you want at the end of a trial in this function
 void AlgorithmA::printDebuggingDetails() {
-    
+    // for (int i = 0; i < capacity; i++)
+	// 	cout << data[i].d << endl;
 }
