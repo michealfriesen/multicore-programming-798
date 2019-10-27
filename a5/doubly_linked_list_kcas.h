fa024774ea149a329501d88f9b9ea18dd56e245b
Micheal Friesen <micheal@mantou.rcs.uwaterloo.ca>
2019-10-27 14:19:07 -0400

    commit

--- a5/doubly_linked_list_kcas.h
+++ a5/doubly_linked_list_kcas.h
@@ -12,11 +12,36 @@ private:
     volatile char padding1[PADDING_BYTES];
 
     class Node {
-        volatile char padding0[PADDING_BYTES];
-        Node * prev; 
-        Node * next;
-        casword_t data;
+        private:
+            volatile char padding0[PADDING_BYTES];
+            Node * prev; 
+            Node * next;
+            casword_t data;
+
+        public:
+
+            Node(Node * p, Node * n, casword_t d) 
+                : prev(p), next(n), data(d) {}
+
+            // Constructor for an empty node that will be connected
+            // at a later point (like the head and tail node)
+            Node(casword_t d) 
+            {
+                data = d;
+                prev = NULL;
+                next = NULL;
+            }
+
+            void setNext(Node * n) {
+                this.next = n;
+            }
+            void setPrev(Node * p) {
+                this.prev = p;
+            }
     };
+
+    Node head;
+    Node tail;
 
 public:
     DoublyLinkedList(const int _numThreads, const int _minKey, const int _maxKey);
@@ -31,14 +56,16 @@ public:
 };
 
 DoublyLinkedList::DoublyLinkedList(const int _numThreads, const int _minKey, const int _maxKey)
-        : numThreads(_numThreads), minKey(_minKey), maxKey(_maxKey) {
+    : numThreads(_numThreads), minKey(_minKey), maxKey(_maxKey){
     // it may be useful to know about / use the "placement new" operator (google)
     // because the simple_record_manager::allocate does not take constructor arguments
     // ... placement new essentially lets you call a constructor after an object already exists.
     
-    // TODO:
-    // Create the max and min nodes
-    // Connect them
+    head = Node(_maxKey);
+    tail = Node(_minKey);
+
+    head.setNext(&tail);
+    tail.setNext(&head);
 }
 
 DoublyLinkedList::~DoublyLinkedList() {
