1ad2e1dd4f0cf182b1bb7cecd5f04f2f8698cd97
Micheal Friesen <m4friese@node06.rift-domain.cs.uwaterloo.ca>
2019-10-30 23:42:31 -0400

    Finally figured out my performance issue

--- a5/doubly_linked_list_kcas.h
+++ a5/doubly_linked_list_kcas.h
@@ -12,27 +12,10 @@ private:
     KCASLockFree<5> kcas;
 
     struct Node {
-        KCASLockFree<1> initKcas;
         casword_t prev; 
         casword_t next;
         int data;
         casword_t mark;
-
-        Node(int tid, Node * p, Node * n, int d) {
-            initKcas.writeInitPtr(tid, &next, (casword_t) n);
-            initKcas.writeInitPtr(tid, &prev, (casword_t) p);
-            data = d;
-            initKcas.writeInitVal(tid, &mark, (casword_t) false);
-        }
-
-        // Constructor for an empty node that will be connected
-        // at a later point (like the head and tail node)
-        Node(int tid, int d) {
-            initKcas.writeInitPtr(tid, &next, (casword_t) NULL);
-            initKcas.writeInitPtr(tid, &prev, (casword_t) NULL);
-            data = d;
-            initKcas.writeInitVal(tid, &mark, (casword_t) false);
-        }
     };
 
     Node * head;
@@ -48,13 +31,29 @@ public:
     
     long getSumOfKeys(); // should return the sum of all keys in the set
     void printDebuggingDetails(); // print any debugging details you want at the end of a trial in this function
+
+    void setupNode(int tid, Node * node, casword_t d) {
+        kcas.writeInitPtr(tid, &node->next, (casword_t) NULL);
+        kcas.writeInitPtr(tid, &node->prev, (casword_t) NULL);
+        node->data = d;
+        kcas.writeInitVal(tid, &node->mark, (casword_t) false);
+    }
+
+    void setupNode(int tid, Node * node, Node * p, Node * n, int d) {
+        kcas.writeInitPtr(tid, &node->next, (casword_t) n);
+        kcas.writeInitPtr(tid, &node->prev, (casword_t) p);
+        node->data = d;
+        kcas.writeInitVal(tid, &node->mark, (casword_t) false);
+    }
 };
 
 DoublyLinkedList::DoublyLinkedList(const int _numThreads, const int _minKey, const int _maxKey)
     : numThreads(_numThreads), minKey(_minKey), maxKey(_maxKey){
     
-    head = new Node(0, minKey);
-    tail = new Node(0, maxKey + 1);
+    head = new Node();
+    setupNode(0, head, minKey);
+    tail = new Node();
+    setupNode(0, tail, maxKey + 1);
     
     // Effectively head->next = tail;
     auto descPtrHead = kcas.getDescriptor(0);
@@ -103,7 +102,8 @@ bool DoublyLinkedList::insertIfAbsent(const int tid, const int & key) {
     };
 
     Node * prevNode = (Node *) kcas.readPtr(tid, &currentNode->prev);
-    Node * newNode = new Node(tid, prevNode, currentNode, key);
+    Node * newNode = new Node();
+    setupNode(tid, newNode, prevNode, currentNode, key);
     
     auto descPtr = kcas.getDescriptor(tid);
     descPtr->addValAddr(&prevNode->mark, false, false);
--- a5/doubly_linked_list_kcas_reclaim.h
+++ a5/doubly_linked_list_kcas_reclaim.h
@@ -15,25 +15,10 @@ private:
     KCASLockFree<5> kcas;
     
     struct Node {
-        KCASLockFree<1> initKcas;
         casword_t prev; 
         casword_t next;
         int data;
         casword_t mark;
-
-        void setup(int tid, casword_t d) {
-            initKcas.writeInitPtr(tid, &next, (casword_t) NULL);
-            initKcas.writeInitPtr(tid, &prev, (casword_t) NULL);
-            data = d;
-            initKcas.writeInitVal(tid, &mark, (casword_t) false);
-        }
-
-        void setup(int tid, Node * p, Node * n, casword_t d) {
-            initKcas.writeInitPtr(tid, &next, (casword_t) n);
-            initKcas.writeInitPtr(tid, &prev, (casword_t) p);
-            data = d;
-            initKcas.writeInitVal(tid, &mark, (casword_t) false);
-        }
     };
     simple_record_manager<Node> * recmgr;
 
@@ -50,6 +35,20 @@ public:
     
     long getSumOfKeys(); // should return the sum of all keys in the set
     void printDebuggingDetails(); // print any debugging details you want at the end of a trial in this function
+
+    void setupNode(int tid, Node * node, casword_t d) {
+        kcas.writeInitPtr(tid, &node->next, (casword_t) NULL);
+        kcas.writeInitPtr(tid, &node->prev, (casword_t) NULL);
+        node->data = d;
+        kcas.writeInitVal(tid, &node->mark, (casword_t) false);
+    }
+
+    void setupNode(int tid, Node * node, Node * p, Node * n, int d) {
+        kcas.writeInitPtr(tid, &node->next, (casword_t) n);
+        kcas.writeInitPtr(tid, &node->prev, (casword_t) p);
+        node->data = d;
+        kcas.writeInitVal(tid, &node->mark, (casword_t) false);
+    }
     
 };
 
@@ -59,10 +58,9 @@ DoublyLinkedListReclaim::DoublyLinkedListReclaim(const int _numThreads, const in
     recmgr->getGuard(0);
 
     head = recmgr->allocate<Node>(0);
-    head->setup(0, minKey);
-        
+    setupNode(0, head, minKey); 
     tail = recmgr->allocate<Node>(0);
-    tail->setup(0, maxKey + 1);
+    setupNode(0, tail, maxKey + 1);
 
     // Effectively head->next = tail;
     auto descPtrHead = kcas.getDescriptor(0);
@@ -128,7 +126,7 @@ bool DoublyLinkedListReclaim::insertIfAbsent(const int tid, const int & key) {
 
     Node * prevNode = (Node *) kcas.readPtr(tid, &currentNode->prev);
     Node * newNode = recmgr->allocate<Node>(tid); 
-    newNode->setup(tid, prevNode, currentNode, key);
+    setupNode(tid, newNode, prevNode, currentNode, key);
     
     auto descPtr = kcas.getDescriptor(tid);
     descPtr->addValAddr(&prevNode->mark, false, false);
